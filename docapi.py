f82a65c5028331b902a66202dc2de5f6552aa9d2
esND <eric.surma@slalom.com>
2017-05-23 08:40:33 -0500

    version 2
    File looping and CSV export

--- docapi.py
+++ docapi.py
@@ -39,57 +39,76 @@ def get_name_resolved_calculation(fieldObj, allFieldsObj):
     return mappedField
 
 
-
-if printWorksheet:
-    for ws in sourceWB.datasources[2:3]:
-        for f in ws.fields.values():
-            print ('######### GETTING: {}'.format(f.name))
-            print (get_name_resolved_calculation(f, ws.fields))
-            # print (get_nested_calculation(ws.fields['CY Label (Percentage)'], ws.fields))
-
-            # print (f.worksheets)
-            # print (f.id, f.name, f.caption)
-            # print (f.calculation)
-
-
-if printDatasource:
-    for sourceTDS in sourceWB.datasources[2:3]:
-        ############################################################
-        # Print out all of the fields and what type they are
-        ############################################################
-        print('----------------------------------------------------------')
-        print('--- fields for {} [{}]'.format(sourceTDS.caption, sourceTDS.name))
-        print('--- {} total fields in this datasource'.format(len(sourceTDS.fields)))
-        print('----------------------------------------------------------')
+import os
+import glob
+import pandas as pd
+import numpy as np
+     
+# Loop through all files with the same file extension #
+d = []
+path = "C:\\Users\\eric.surma\\Documents\\Tools\\Tableau\\Python\\samples/*.twbx"
+for fname in glob.glob(path):
+    sourceWB = Workbook(fname)
+    for sourceTDS in sourceWB.datasources:
         for count, field in enumerate(sourceTDS.fields.values()):
-            print('{:>4}: {} is a {}'.format(count+1, field.name, field.datatype))
-            blank_line = False
-            if field.calculation:
-                print('      the formula is {}'.format(field.calculation.encode('utf-8').strip()))
-                blank_line = True
-            if field.default_aggregation:
-                print('      the default aggregation is {}'.format(field.default_aggregation))
-                blank_line = True
-            if field.description:
-                print('      the description is {}'.format(field.description))
-
-            if blank_line:
-                print('')
-        print('----------------------------------------------------------')
+            d.append({'Field': field.name, 'Type': field.datatype, 'Default Aggregation': field.default_aggregation,
+                      'Field Calculation': field.calculation})
+d = pd.DataFrame(d)
+d.to_csv('test.csv') # Output File
 
 
-if saveExcel:
-    book = xlsxwriter.Workbook('pythonexcel.xlsx')
-    sh = book.add_worksheet()
-    sh.set_column('A:A',20)
-    sh.write(0,0,'Calculation Name')
-    for ws in sourceWB.datasources[2:3]:
-        row = 1
-        for f in ws.fields.values():
-            sh.write(row,0,str(f.worksheets))
-            sh.write(row,1,f.name)
-            sh.write(row,2,f.caption)
-            sh.write(row,3,f.calculation)
-            row+=1
-
-    book.close()
+#==============================================================================
+# if printWorksheet:
+#     for ws in sourceWB.datasources[2:3]:
+#         for f in ws.fields.values():
+#             print ('######### GETTING: {}'.format(f.name))
+#             print (get_name_resolved_calculation(f, ws.fields))
+#             # print (get_nested_calculation(ws.fields['CY Label (Percentage)'], ws.fields))
+# 
+#             # print (f.worksheets)
+#             # print (f.id, f.name, f.caption)
+#             # print (f.calculation)
+# 
+# 
+# if printDatasource:
+#     for sourceTDS in sourceWB.datasources[2:3]:
+#         ############################################################
+#         # Print out all of the fields and what type they are
+#         ############################################################
+#         print('----------------------------------------------------------')
+#         print('--- fields for {} [{}]'.format(sourceTDS.caption, sourceTDS.name))
+#         print('--- {} total fields in this datasource'.format(len(sourceTDS.fields)))
+#         print('----------------------------------------------------------')
+#         for count, field in enumerate(sourceTDS.fields.values()):
+#             print('{:>4}: {} is a {}'.format(count+1, field.name, field.datatype))
+#             blank_line = False
+#             if field.calculation:
+#                 print('      the formula is {}'.format(field.calculation.encode('utf-8').strip()))
+#                 blank_line = True
+#             if field.default_aggregation:
+#                 print('      the default aggregation is {}'.format(field.default_aggregation))
+#                 blank_line = True
+#             if field.description:
+#                 print('      the description is {}'.format(field.description))
+# 
+#             if blank_line:
+#                 print('')
+#         print('----------------------------------------------------------')
+# 
+# 
+# if saveExcel:
+#     book = xlsxwriter.Workbook('pythonexcel.xlsx')
+#     sh = book.add_worksheet()
+#     sh.set_column('A:A',20)
+#     sh.write(0,0,'Calculation Name')
+#     for ws in sourceWB.datasources[2:3]:
+#         row = 1
+#         for f in ws.fields.values():
+#             sh.write(row,0,str(f.worksheets))
+#             sh.write(row,1,f.name)
+#             sh.write(row,2,f.caption)
+#             sh.write(row,3,f.calculation)
+#             row+=1
+# 
+#     book.close()
+#==============================================================================
