356ca6dbed0539dfc5408406f2dab17f8736ea38
Scott <scottishs@gmail.com>
2018-03-10 11:47:16 -0600

    Long delay commit
    Catching up on the commit, since we haven't worked on this in forevever. No idea of what changed.

--- docapi.py
+++ docapi.py
@@ -40,6 +40,78 @@ def get_name_resolved_calculation(fieldObj, allFieldsObj):
             mappedField = mappedField.replace(toMap, allFieldsObj[toMap].caption)
     return mappedField
 
+def _resolve_calculation_parts(fieldObj, allFieldsObj, resolutionType):
+    #TODO need to make this loop through Calculation_ and other fields that have names, but need to be resolved
+
+    #this is the target field which we're resoliving calculation names to
+    mappedField = fieldObj.calculation
+    while mappedField.find('[') >= 0:
+        n = mappedField.find('[') #starting point of calc
+        toMap = mappedField[n:mappedField.find(']', n)+1] #find the end of the calc name
+
+        #set output to the specified resolutionType
+        if resolutionType is None or resolutionType == 'calc':
+            rfield = allFieldsObj[toMap].calculation
+        elif resolutionType == 'caption':
+            rfield = allFieldsObj[toMap].caption
+        elif resolutionType == 'calcwithref':
+            rfield = '{[{}] {}}'.format(allFieldsObj[toMap].caption, allFieldsObj[toMap].calculation)
+        else:
+            rfield = 'could not resolve type'
+            print ("invalid parameter [{}] passed for 'resolutionType' ".format(resolutionType))
+
+        mappedField = mappedField.replace(toMap, rfield)
+    return mappedField
+
+
+def _get_calc_value(fieldObj, type):
+    return getattr(fieldObj, type)
+
+def _field_has_calculation(fieldObj):
+    return fieldObj.calculation is not None
+
+
+
+
+def get_resolved_calculation(fieldObj, allFieldsObj, resolutionType=None):
+    """ returns calculation caption of a field, with resolved nested calculations """
+    """ options: calc (default); caption; calcwithref """
+    if not _field_has_calculation(fieldObj):
+        return None
+
+    print 'checker: ', fieldObj.name, _field_has_calculation(fieldObj)
+
+    ## Resolve non-named calculated fields
+    # look for the fields with 'Calculation_' ids
+    #TODO make the function call to resolve the names
+    #this is the target field which we're resoliving calculation names to
+    mappedField = fieldObj.calculation
+    while mappedField.find('[') >= 0:
+        n = mappedField.find('[') #starting point of calc
+        toMap = mappedField[n:mappedField.find(']', n)+1] #find the end of the calc name
+
+        #set output to the specified resolutionType
+        #TODO need to check if the field is a calculation or just an attributes
+        if resolutionType is None or resolutionType == 'calc':
+            # if toMap == '[Returned]':
+            #     print ('RETURN FOUND: PRINTING OBJECTS IN LIST')
+            #     for x in allFieldsObj:
+            #         print x, x[0]
+            # print "GETTING: ", toMap, ' ,', allFieldsObj[toMap]," | RESULT: ", _get_calc_value(allFieldsObj[toMap], 'name')
+            rfield = _get_calc_value(allFieldsObj[toMap], 'name')
+            # rfield = allFieldsObj[toMap].calculation
+        # elif resolutionType == 'caption':
+        #     rfield = allFieldsObj[toMap].caption
+        # elif resolutionType == 'calcwithref':
+        #     rfield = '{[{}] {}}'.format(allFieldsObj[toMap].caption, allFieldsObj[toMap].calculation)
+        # else:
+        #     rfield = 'could not resolve type'
+        #     print ("invalid parameter [{}] passed for 'resolutionType' ".format(resolutionType))
+        # print toMap
+        mappedField = mappedField.replace(toMap, 'rfield')
+    return mappedField
+    # return _resolve_calculation_parts(fieldObj, allFieldsObj, resolutionType)
+
 
 
 
@@ -47,60 +119,65 @@ def get_name_resolved_calculation(fieldObj, allFieldsObj):
 d = []
 dw = []
 
+##### TODO Re-enable this
 path = ""
 if path == "":
     path = os.path.realpath(os.getcwd())
 
 for fname in os.listdir(path):
-    # if fname == 'Sample Superstore Workbook ALT.twb':
-    if fname.endswith('twb') or fname.endswith('twbx') or fname.endswith('tds'):
-        sourceWB = Workbook(fname)
-        for sourceTDS in sourceWB.datasources:
-            if sourceTDS.name != 'Parameters':
-                for count, field in enumerate(sourceTDS.fields.values()):
-                    d.append({'Workbook': sourceWB.filename,
-                            'Field': field.name,
-                            'Type': field.datatype,
-                            'Default Aggregation': field.default_aggregation,
-                            'Field Calculation': get_calc_resolved_calculation(field, sourceTDS.fields)
-                            })
+    if fname == 'Sample Superstore Workbook ALT.twb':
+        if fname.endswith('twb') or fname.endswith('twbx') or fname.endswith('tds'):
+            sourceWB = Workbook(fname)
+            for sourceTDS in sourceWB.datasources:
+                if sourceTDS.name != 'Parameters':
+                    # for x in sourceTDS.fields:
+                    #     print "RAISING 1: TDS Name: {}, Caption: {}, Value: {}".format(sourceTDS.name, sourceTDS.caption, x)
+
+                    for count, field in enumerate(sourceTDS.fields.values()):
+                        print "WORKBOOK: ", sourceWB.filename, " DATASOURCE: ", sourceTDS.name, " FIELD: ", field.name
+                        d.append({'Workbook': sourceWB.filename,
+                                'Field': field.name,
+                                'Type': field.datatype,
+                                'Default Aggregation': field.default_aggregation,
+                                'Field Calculation': get_resolved_calculation(field, sourceTDS.fields)
+                                })
 
 d = pd.DataFrame(d)
 
 ########################################
 # De-Duplication Unique List of Fields #
 ########################################
-
-d1 = d # Create a Copy
-d1['Field'] = d1['Field'].str.strip('[]') # Remove Brackets [] from field names
-d1 = d1.drop_duplicates(subset=['Default Aggregation', 'Field', 'Field Calculation','Type'])  # Ignore Workbook                  
-d1 = d1.sort_values('Field') # Sort for convenience                       
-d1['freq'] = d1.groupby('Field')['Field'].transform('count') # Add Frequency
-d1_1 = d1.query('freq > 1') # Partition Fields with duplicates
-d1_2 = d1.query('freq == 1') # Partition Fields without duplicates             
-               
-d1_1['Field_Calc_Count'] = d1_1.groupby(['Field'])['Field Calculation'].transform('count')
-
-d1_1_1 = d1_1.query('Field_Calc_Count > 1') # Multipe fields, diff calcs
-d1_1_1['Dup_Calc'] = 1
-                   
-d1_1_2 = d1_1.query('Field_Calc_Count <=1') # Non duplicate field calcs
-
-d1_1_2['Agg_Null'] = np.where(d1_1_2['Default Aggregation'].isnull(),1,0)
-d1_1_2 = d1_1_2.query('Agg_Null==0') # Non Duplicates
-
-# Join final partitions together # 
-final = pd.concat([d1_2, d1_1_2, d1_1_1])
-
-# Check for Same Calc with different field names #                  
-final['calc_freq'] = final.groupby('Field Calculation')['Field Calculation'].transform('count')
-final['Dup_Field'] = np.where(final['calc_freq']>1,1,"")
-
-final.to_csv('Python Output File.csv'
-        , columns=[#'Workbook', 
-        'Field', 'Type', 'Default Aggregation', 'Field Calculation',
-        'Dup_Calc','Dup_Field']
-        ) # Output File
+#TODO Re-enable this
+# d1 = d # Create a Copy
+# d1['Field'] = d1['Field'].str.strip('[]') # Remove Brackets [] from field names
+# d1 = d1.drop_duplicates(subset=['Default Aggregation', 'Field', 'Field Calculation','Type'])  # Ignore Workbook
+# d1 = d1.sort_values('Field') # Sort for convenience
+# d1['freq'] = d1.groupby('Field')['Field'].transform('count') # Add Frequency
+# d1_1 = d1.query('freq > 1') # Partition Fields with duplicates
+# d1_2 = d1.query('freq == 1') # Partition Fields without duplicates
+#
+# d1_1['Field_Calc_Count'] = d1_1.groupby(['Field'])['Field Calculation'].transform('count')
+#
+# d1_1_1 = d1_1.query('Field_Calc_Count > 1') # Multipe fields, diff calcs
+# d1_1_1['Dup_Calc'] = 1
+#
+# d1_1_2 = d1_1.query('Field_Calc_Count <=1') # Non duplicate field calcs
+#
+# d1_1_2['Agg_Null'] = np.where(d1_1_2['Default Aggregation'].isnull(),1,0)
+# d1_1_2 = d1_1_2.query('Agg_Null==0') # Non Duplicates
+#
+# # Join final partitions together #
+# final = pd.concat([d1_2, d1_1_2, d1_1_1])
+#
+# # Check for Same Calc with different field names #
+# final['calc_freq'] = final.groupby('Field Calculation')['Field Calculation'].transform('count')
+# final['Dup_Field'] = np.where(final['calc_freq']>1,1,"")
+#
+# final.to_csv('Python Output File.csv'
+#         , columns=[#'Workbook',
+#         'Field', 'Type', 'Default Aggregation', 'Field Calculation',
+#         'Dup_Calc','Dup_Field']
+#         ) # Output File
 
 
 
